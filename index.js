bb1f6f586fe5d304a815eefcf854eb6ed623c84b
Nicholas A Christensen <nickachristensen@gmail.com>
2021-08-09 13:21:55 -0400

    commit tree d4c137bf8cb7ab99df18b5215f685711c2f61a80

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+const num1 = 31;
 
-// Write your code here
+const num2 = 2;
+
+const num3 = 19;
+
+const num4 = 5;
+
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10);
+
+let mod = num3 % num4;
+    
+let max = Math.max(15, 16 , 17 , 18, 19, 20);
